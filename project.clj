a491158adaaaea32bf30335f7787eafe9d54a2c9
Wilker Lucio <wilkerlucio@gmail.com>
2017-12-07 21:44:26 -0200

    Bump fulcro and main version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fulcrologic/fulcro-inspect "0.2.0-SNAPSHOT"
+(defproject fulcrologic/fulcro-inspect "2.0.0-alpha1-SNAPSHOT"
   :description "A debugging tool for Fulcro that allows you to inspect state and other aspects of the running application(s)."
   :url "https://github.com/fulcrologic/fulcro-inspect"
   :min-lein-version "2.7.0"
@@ -7,7 +7,7 @@
   :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
                  [org.clojure/clojurescript "1.9.946" :scope "provided"]
                  [clojure-future-spec "1.9.0-beta4"]
-                 [fulcrologic/fulcro "2.0.0-beta4"]
+                 [fulcrologic/fulcro "2.0.0-beta5"]
                  [fulcrologic/fulcro-css "2.0.0-beta1"]
 
                  [org.clojure/tools.namespace "0.3.0-alpha4" :scope "test"]
