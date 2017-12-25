aa22bccbdf267a37df1fcdadd522ff8ab44c0009
Wilker Lucio <wilkerlucio@gmail.com>
2017-12-25 16:47:02 -0200

    Bumpp fulcr css and release for alpha-3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fulcrologic/fulcro-inspect "2.0.0-alpha3-SNAPSHOT"
+(defproject fulcrologic/fulcro-inspect "2.0.0-alpha3"
   :description "A debugging tool for Fulcro that allows you to inspect state and other aspects of the running application(s)."
   :url "https://github.com/fulcrologic/fulcro-inspect"
   :min-lein-version "2.7.0"
@@ -7,8 +7,8 @@
   :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
                  [org.clojure/clojurescript "1.9.946" :scope "provided"]
                  [clojure-future-spec "1.9.0-beta4"]
-                 [fulcrologic/fulcro "2.0.0-RC3"]
-                 [fulcrologic/fulcro-css "2.0.0-beta1"]
+                 [fulcrologic/fulcro "2.0.0-RC3" :scope "provided"]
+                 [fulcrologic/fulcro-css "2.0.0"]
 
                  [org.clojure/tools.namespace "0.3.0-alpha4" :scope "test"]
                  [lein-doo "0.1.7" :scope "test"]
