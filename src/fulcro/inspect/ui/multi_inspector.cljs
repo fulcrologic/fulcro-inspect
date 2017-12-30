6935e600c8534d77250e9fe956b00ed1aa90c50f
Wilker Lucio <wilkerlucio@gmail.com>
2017-12-30 17:46:16 -0200

    Make multi-inspector use defsc

--- src/fulcro/inspect/ui/multi_inspector.cljs
+++ src/fulcro/inspect/ui/multi_inspector.cljs
@@ -23,59 +23,49 @@
     (let [{:keys [ref state]} env]
       (swap! state update-in ref assoc ::current-app [::inspector/id id]))))
 
-(fp/defui ^:once MultiInspector
-  static fp/InitialAppState
-  (initial-state [_ _] {::inspectors  []
-                        ::current-app nil})
+(fp/defsc MultiInspector [this {::keys [inspectors current-app]} _ css]
+  {:initial-state (fn [_] {::inspectors  []
+                           ::current-app nil})
 
-  static fp/Ident
-  (ident [_ props] [::multi-inspector "main"])
+   :ident         (fn [] [::multi-inspector "main"])
+   :query         [::inspectors {::current-app (fp/get-query inspector/Inspector)}]
+   :css           [[:.container {:display        "flex"
+                                 :flex-direction "column"
+                                 :width          "100%"
+                                 :height         "100%"
+                                 :overflow       "hidden"}]
+                   [:.selector {:font-family ui/label-font-family
+                                :font-size   ui/label-font-size
+                                :display     "flex"
+                                :align-items "center"
+                                :background  "#f3f3f3"
+                                :color       ui/color-text-normal
+                                :border-top  "1px solid #ccc"
+                                :padding     "12px"
+                                :user-select "none"}]
+                   [:.label {:margin-right "10px"}]
+                   [:.no-app {:display         "flex"
+                              :background      "#f3f3f3"
+                              :font-family     ui/label-font-family
+                              :font-size       "23px"
+                              :flex            1
+                              :align-items     "center"
+                              :justify-content "center"}]]
+   :css-include   [inspector/Inspector]}
 
-  static fp/IQuery
-  (query [_] [::inspectors {::current-app (fp/get-query inspector/Inspector)}])
-
-  static css/CSS
-  (local-rules [_] [[:.container {:display        "flex"
-                                  :flex-direction "column"
-                                  :width          "100%"
-                                  :height         "100%"
-                                  :overflow       "hidden"}]
-                    [:.selector {:font-family ui/label-font-family
-                                 :font-size   ui/label-font-size
-                                 :display     "flex"
-                                 :align-items "center"
-                                 :background  "#f3f3f3"
-                                 :color       ui/color-text-normal
-                                 :border-top  "1px solid #ccc"
-                                 :padding     "12px"
-                                 :user-select "none"}]
-                    [:.label {:margin-right "10px"}]
-                    [:.no-app {:display         "flex"
-                               :background      "#f3f3f3"
-                               :font-family     ui/label-font-family
-                               :font-size       "23px"
-                               :flex            1
-                               :align-items     "center"
-                               :justify-content "center"}]])
-  (include-children [_] [inspector/Inspector])
-
-  Object
-  (render [this]
-    (let [{::keys [inspectors current-app]} (fp/props this)
-          css (css/get-classnames MultiInspector)]
-      (dom/div #js {:className (:container css)}
-        (if current-app
-          (inspector/inspector current-app)
-          (dom/div #js {:className (:no-app css)}
-            (dom/div nil "No app connected.")))
-        (if (> (count inspectors) 1)
-          (dom/div #js {:className (:selector css)}
-            (dom/div #js {:className (:label css)} "App")
-            (dom/select #js {:value    (str (::inspector/id current-app))
-                             :onChange #(fp/transact! this `[(set-app {::inspector/id ~(read-string (.. % -target -value))})])}
-              (for [app-id (->> (map (comp pr-str second) inspectors) sort)]
-                (dom/option #js {:key   app-id
-                                 :value app-id}
-                  app-id)))))))))
+  (dom/div #js {:className (:container css)}
+    (if current-app
+      (inspector/inspector current-app)
+      (dom/div #js {:className (:no-app css)}
+        (dom/div nil "No app connected.")))
+    (if (> (count inspectors) 1)
+      (dom/div #js {:className (:selector css)}
+        (dom/div #js {:className (:label css)} "App")
+        (dom/select #js {:value    (str (::inspector/id current-app))
+                         :onChange #(fp/transact! this `[(set-app {::inspector/id ~(read-string (.. % -target -value))})])}
+          (for [app-id (->> (map (comp pr-str second) inspectors) sort)]
+            (dom/option #js {:key   app-id
+                             :value app-id}
+              app-id)))))))
 
 (def multi-inspector (fp/factory MultiInspector {:keyfn ::multi-inspector}))
