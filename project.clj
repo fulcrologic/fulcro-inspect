a6eca2ac8cbbc4fef618a44f2cbb6679725e3abf
Wilker Lucio <wilkerlucio@gmail.com>
2018-05-16 08:55:41 -0300

    Setup shadow compilation

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fulcrologic/fulcro-inspect "2.1.0-SNAPSHOT"
+(defproject fulcrologic/fulcro-inspect "2.1.0"
   :description "A debugging tool for Fulcro that allows you to inspect state and other aspects of the running application(s)."
   :url "https://github.com/fulcrologic/fulcro-inspect"
   :min-lein-version "2.7.0"
