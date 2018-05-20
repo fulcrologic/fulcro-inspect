bdbbd66e589f25aad4089b2237f1ae9fd57bdaa4
Wilker Lucio <wilkerlucio@gmail.com>
2018-05-20 02:03:29 -0300

    Hook network inspection

--- src/remote/fulcro/inspect/remote.cljs
+++ src/remote/fulcro/inspect/remote.cljs
@@ -4,7 +4,9 @@
             [fulcro.inspect.remote.transit :as encode]
             [goog.object :as gobj]
             [fulcro.inspect.ui.data-history :as data-history]
-            [clojure.set :as set]))
+            [clojure.set :as set]
+            [fulcro.inspect.ui.network :as network]
+            [fulcro.client.network :as f.network]))
 
 (defonce started?* (atom false))
 (defonce apps* (atom {}))
@@ -50,8 +52,8 @@
 (defn inspect-app [target-app]
   (let [state* (some-> target-app :reconciler :config :state)
         app-id (app-name (:reconciler target-app))]
-    #_(inspect-network-init (-> target-app :networking :remote) {:inspector inspector
-                                                                 :app       target-app})
+
+    (inspect-network-init (-> target-app :networking :remote) target-app)
 
     (add-watch state* app-id
       #(update-inspect-state app-id %4))
@@ -68,6 +70,101 @@
       (if (-> reconciler fp/app-state deref ::initialized)
         (transact! [:fulcro.inspect.ui.transactions/tx-list-id [app-id-key app-id]]
                    [`(fulcro.inspect.ui.transactions/add-tx ~tx) :fulcro.inspect.ui.transactions/tx-list])))))
+
+;;; network
+
+(defrecord TransformNetwork [network options]
+  f.network/NetworkBehavior
+  (serialize-requests? [this]
+    (try
+      (f.network/serialize-requests? network)
+      (catch :default _ true)))
+
+  f.network/FulcroNetwork
+  (send [_ edn ok error]
+    (let [{::keys [transform-query transform-response transform-error app*]
+           :or    {transform-query    (fn [_ x] x)
+                   transform-response (fn [_ x] x)
+                   transform-error    (fn [_ x] x)}} options
+          req-id (random-uuid)
+          env    {::request-id req-id
+                  ::app        @app*}]
+      (if-let [edn' (transform-query env edn)]
+        (f.network/send network edn'
+          #(->> % (transform-response env) ok)
+          #(->> % (transform-error env) error))
+        (ok nil))))
+
+  (start [this]
+    (try
+      (f.network/start network)
+      (catch ::default e
+        (js/console.log "Error starting sub network" e)))
+    this))
+
+(defn transform-network [network options]
+  (->TransformNetwork network (assoc options ::app* (atom nil))))
+
+(defrecord TransformNetworkI [network options]
+  f.network/FulcroRemoteI
+  (transmit [_ {::f.network/keys [edn ok-handler error-handler]}]
+    (let [{::keys [transform-query transform-response transform-error app*]
+           :or    {transform-query    (fn [_ x] x)
+                   transform-response (fn [_ x] x)
+                   transform-error    (fn [_ x] x)}} options
+          req-id (random-uuid)
+          env    {::request-id req-id
+                  ::app        @app*}]
+      (if-let [edn' (transform-query env edn)]
+        (f.network/transmit network
+          {::f.network/edn           edn'
+           ::f.network/ok-handler    #(->> % (transform-response env) ok-handler)
+           ::f.network/error-handler #(->> % (transform-error env) error-handler)})
+        (ok-handler nil))))
+
+  (abort [_ abort-id] (f.network/abort network abort-id)))
+
+(defn transform-network-i [network options]
+  (->TransformNetworkI network (assoc options ::app* (atom nil))))
+
+(defn inspect-network
+  ([remote network]
+   (let [ts {::transform-query
+             (fn [{::keys [request-id app]} edn]
+               (let [app-id (app-name (:reconciler app))]
+                 (transact! [::network/history-id [app-id-key app-id]]
+                            [`(network/request-start ~{::network/remote      remote
+                                                       ::network/request-id  request-id
+                                                       ::network/request-edn edn})]))
+               edn)
+
+             ::transform-response
+             (fn [{::keys [request-id app]} response]
+               (let [app-id (app-name (:reconciler app))]
+                 (transact! [::network/history-id [app-id-key app-id]]
+                            [`(network/request-finish ~{::network/request-id   request-id
+                                                        ::network/response-edn response})]))
+               response)
+
+             ::transform-error
+             (fn [{::keys [request-id app]} error]
+               (let [app-id (app-name (:reconciler app))]
+                 (transact! [::network/history-id [app-id-key app-id]]
+                            [`(network/request-finish ~{::network/request-id request-id
+                                                        ::network/error      error})]))
+               error)}]
+     (cond
+       (implements? f.network/FulcroNetwork network)
+       (transform-network network ts)
+
+       (implements? f.network/FulcroRemoteI network)
+       (transform-network-i network
+         (update ts ::transform-response (fn [tr] (fn [env {:keys [body] :as response}]
+                                                    (tr env body)
+                                                    response))))
+
+       :else
+       (js/console.warn "Invalid network" {:network network})))))
 
 (defn install [_]
   (js/document.documentElement.setAttribute "__fulcro-inspect-remote-installed__" true)
@@ -95,9 +192,9 @@
            (inspect-app app))
          app)
 
-       #_#_::fulcro/network-wrapper
-           (fn [networks]
-             (into {} (map (fn [[k v]] [k (inspect-network k v)])) networks))
+       ::fulcro/network-wrapper
+       (fn [networks]
+         (into {} (map (fn [[k v]] [k (inspect-network k v)])) networks))
 
        ::fulcro/tx-listen
        inspect-tx})
