69a9cd86e66ab97d6b3dca52f801a1fa06f51d84
Wilker Lucio <wilkerlucio@gmail.com>
2018-09-17 11:43:53 -0300

    New cards and deps management

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fulcrologic/fulcro-inspect "2.2.2"
+(defproject fulcrologic/fulcro-inspect "2.2.3"
   :description "A debugging tool for Fulcro that allows you to inspect state and other aspects of the running application(s)."
   :url "https://github.com/fulcrologic/fulcro-inspect"
   :min-lein-version "2.7.0"
@@ -6,8 +6,8 @@
             :url  "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
                  [org.clojure/clojurescript "1.10.238" :scope "provided"]
-                 [fulcrologic/fulcro "2.5.7" :scope "provided"]
-                 [com.wsscode/pathom "2.0.4"]
+                 [fulcrologic/fulcro "2.6.0" :scope "provided"]
+                 [com.wsscode/pathom "2.2.0-beta4"]
                  [org.clojure/tools.namespace "0.3.0-alpha4" :scope "test"]
                  [lein-doo "0.1.7" :scope "test"]
                  [fulcrologic/fulcro-spec "2.1.0-1" :scope "test" :exclusions [fulcrologic/fulcro]]]
@@ -29,7 +29,7 @@
                              [lein-doo "0.1.8"]]
                    :source-paths ["src/client" "src/chrome" "src/electron" "src/ui" "devcards" "workspaces"]
                    :dependencies [[devcards "0.2.3" :exclusions [cljsjs/react cljsjs/react-dom]]
-                                  [nubank/workspaces "1.0.0-preview6"]
+                                  [nubank/workspaces "1.0.0-preview8"]
                                   [figwheel-sidecar "0.5.14" :exclusions [org.clojure/tools.nrepl]]
                                   [binaryage/devtools "0.9.9"]
                                   [com.cemerick/piggieback "0.2.2"]
--- workspaces/fulcro/inspect/workspaces/main.cljs
+++ workspaces/fulcro/inspect/workspaces/main.cljs
@@ -1,4 +1,7 @@
 (ns fulcro.inspect.workspaces.main
-  (:require [nubank.workspaces.core :as ws]))
+  (:require [nubank.workspaces.core :as ws]
+            [com.wsscode.pathom.viz.trace-cards]
+            [fulcro.inspect.workspaces.ui.network-cards]
+            [fulcro.inspect.workspaces.ui.pathom-trace-cards]))
 
 (ws/mount)
