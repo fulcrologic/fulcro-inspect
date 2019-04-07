2c50888d092e3f5b570583afbfb601d3e033e852
Wilker Lucio <wilkerlucio@gmail.com>
2019-04-07 10:29:12 -0300

    Switch between remotes on index explorer

--- src/ui/fulcro/inspect/ui/index_explorer.cljs
+++ src/ui/fulcro/inspect/ui/index_explorer.cljs
@@ -1,32 +1,37 @@
 (ns fulcro.inspect.ui.index-explorer
   (:require [com.wsscode.pathom.viz.index-explorer :as iex]
+            [cljs.reader :refer [read-string]]
             [fulcro.client.primitives :as fp]
             [fulcro.client.localized-dom :as dom]
             [fulcro.client.data-fetch :as df]
-            [fulcro.inspect.helpers :as db.h]))
+            [fulcro.inspect.helpers :as db.h]
+            [fulcro.client.mutations :as fm]
+            [fulcro.inspect.ui.core :as cui]))
+
+(defn explorer->remote [{::iex/keys [id]}]
+  (if (vector? id) (peek id) id))
 
 (defn load-index [this id]
-  (let [app-id (try (db.h/comp-app-uuid this) (catch :default _))]
+  (let [app-id (try (db.h/comp-app-uuid this) (catch :default _))
+        remote (explorer->remote {::iex/id id})]
     (df/load this [::iex/id id] iex/IndexExplorer
-      {:target       (conj (fp/get-ident this) ::explorer)
-       :update-query (fn [query]
+      {:update-query (fn [query]
                        (into (with-meta [] (meta query))
                              (map (fn [x]
                                     (if (= x ::iex/index)
                                       (list ::iex/index {:fulcro.inspect.core/app-uuid app-id
-                                                         :fulcro.inspect.client/remote :remote})
+                                                         :fulcro.inspect.client/remote remote})
                                       x)))
                              query))})))
 
 (fp/defsc IndexExplorer
-  [this {::keys [explorer] :as props}]
+  [this {::keys [id explorer explorers] :as props} {}]
   {:initial-state (fn [{:keys [app-uuid remotes]}]
                     (let [explorers (mapv
-                                      #(-> {::iex/id [:fulcro.inspect.core/app-uuid app-uuid %]
-                                            :remote  %})
+                                      #(-> {::iex/id [:fulcro.inspect.core/app-uuid app-uuid %]})
                                       remotes)]
                       {::id        [:fulcro.inspect.core/app-uuid app-uuid]
-                       ::active    (first explorers)
+                       ::explorer  (first explorers)
                        ::explorers explorers}))
    :ident         [::id ::id]
    :query         [::id
@@ -50,23 +55,36 @@
                              :align-items     "center"
                              :justify-content "center"
                              :color           "#fff"
-                             :font-size       "21px"}]]}
-  (dom/div :.container
-    (dom/div :.title
-      #_(if (> (count remotes) 1)
+                             :font-size       "21px"}]
+                   [:.remote-selector {:display     "flex"
+                                       :align-items "center"
+                                       :font-family cui/label-font-family
+                                       :font-size   cui/label-font-size
+                                       :color       cui/color-text-normal}]
+                   [:.remote-selector-label {:margin-right "8px"
+                                             :margin-left  "4px"}]]}
+  (let [remotes  (mapv explorer->remote explorers)
+        remote   (explorer->remote explorer)
+        app-uuid (peek id)]
+    (dom/div :.container
+      (dom/div :.title
+        (if (> (count remotes) 1)
           (dom/div :.remote-selector
             (dom/div :.remote-selector-label "Remote:")
-            (dom/select {:onChange #(on-switch-remote (read-string (.. % -target -value)))
+            (dom/select {:onChange #(fm/set-value! this ::explorer
+                                      [::iex/id
+                                       [:fulcro.inspect.core/app-uuid app-uuid
+                                        (read-string (fm/target-value %))]])
                          :value    (pr-str remote)}
               (for [r remotes]
                 (dom/option {:key (pr-str r) :value (pr-str r)} (pr-str r))))))
-      (dom/div :.flex)
-      (dom/button {:onClick #(load-index this (random-uuid))}
-        "Load index"))
-    (if explorer
-      (if (::iex/index explorer)
-        (iex/index-explorer explorer)
-        (dom/div :.empty "Seems like the index is not available."))
-      (dom/div :.empty "No data"))))
+        (dom/div :.flex)
+        (dom/button {:onClick #(load-index this [:fulcro.inspect.core/app-uuid app-uuid remote])}
+          "Load index"))
+      (if explorer
+        (if (::iex/index explorer)
+          (iex/index-explorer explorer)
+          (dom/div :.empty "Seems like the index is not available."))
+        (dom/div :.empty "No data")))))
 
-(def index-explorer (fp/factory IndexExplorer))
+(def index-explorer (fp/computed-factory IndexExplorer))
