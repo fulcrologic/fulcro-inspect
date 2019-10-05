743682c7fdeb8c592f10db3f964c2ae6f8496fec
Tony Kay <tony.kay@gmail.com>
2019-10-04 18:50:33 -0700

    got inspect working in one direction

--- src/electron/fulcro/inspect/electron/background/main.cljs
+++ src/electron/fulcro/inspect/electron/background/main.cljs
@@ -1,22 +1,24 @@
 (ns fulcro.inspect.electron.background.main
   (:require
     [fulcro.inspect.electron.background.server :as server]
-    ["electron" :as electron]
+    ["electron" :as electron :refer [ipcMain]]
     ["path" :as path]
     ["url" :as url]))
+
+(defonce contents (atom nil))
 
 (defn create-window []
   (let [win (electron/BrowserWindow. #js {:width 800 :height 600})]
     (.loadURL win (url/format #js {:pathname (path/join js/__dirname ".." ".." "index.html")
                                    :protocol "file:"
                                    :slashes  "true"}))
-
-    (.. win -webContents openDevTools)))
+    (.. win -webContents openDevTools)
+    (reset! contents (.-webContents win))))
 
 (defn init []
   (js/console.log "start")
   (electron/app.on "ready" create-window)
-  (server/start! {}))
+  (server/start! {:content-atom contents}))
 
 (defn done []
   (js/console.log "Done reloading"))
--- src/electron/fulcro/inspect/electron/background/server.cljs
+++ src/electron/fulcro/inspect/electron/background/server.cljs
@@ -1,22 +1,29 @@
 (ns fulcro.inspect.electron.background.server
   (:require
     [cljs.nodejs :as nodejs]
+    ["electron" :refer [ipcMain]]
+    [goog.object :as gobj]
     [fulcro.inspect.remote.transit :as encode]))
 
 (defonce Server (nodejs/require "socket.io"))
 (goog-define SERVER_PORT 8237)
 
-(defn process-client-message [client msg reply-fn]
-  (js/console.log  (pr-str msg)))
+(defn process-client-message [web-contents msg reply-fn]
+  (js/console.log "forwarding ws message to rendering client")
+  (try
+    (when web-contents
+      (.send web-contents "event" #js {"fulcro-inspect-remote-message" msg}))
+    (catch :default e
+      (js/console.error e))))
 
-(defn start! [{::keys [on-ready on-connect]}]
+(defn start! [{:keys [content-atom]}]
   (let [io (Server)]
     (js/console.log "Starting websocket server on port " SERVER_PORT)
     (.on io "connection" (fn [client]
                            (.on client "event"
                              (fn [data reply-fn]
-                               (let [msg (some-> data encode/read)]
-                                 (process-client-message client msg reply-fn))))
+                               (when-let [web-contents (some-> content-atom deref)]
+                                 (process-client-message web-contents data reply-fn))))
                            (js/console.log "Client connected")))
     (.listen io SERVER_PORT)))
 
