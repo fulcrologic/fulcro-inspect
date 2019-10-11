286aaa30da47590bba96544123e5d32606dcec9c
Wilker Lúcio <wilkerlucio@users.noreply.github.com>
2019-10-11 11:43:15 -0300

    Bumps (#74)
    * Bump pathom and shadow
    
    * Bump electron
    
    * Remove ascii encoding on electron

--- src/electron/fulcro/inspect/electron/background/main.cljs
+++ src/electron/fulcro/inspect/electron/background/main.cljs
@@ -8,7 +8,9 @@
 (defonce contents (atom nil))
 
 (defn create-window []
-  (let [win (electron/BrowserWindow. #js {:width 800 :height 600})]
+  (let [win (electron/BrowserWindow. #js {:width          800
+                                          :height         600
+                                          :webPreferences #js {:nodeIntegration true}})]
     (.loadURL win (url/format #js {:pathname (path/join js/__dirname ".." ".." "index.html")
                                    :protocol "file:"
                                    :slashes  "true"}))
