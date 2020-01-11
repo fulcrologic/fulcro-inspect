6e594d08d8b9c4303ed802d45632e430fa4f193c
Anthony D'Ambrosio <anthony.dayzerostudio@gmail.com>
2020-01-10 20:25:43 -0800

    first pass -> html table rendering & ident as links

--- src/ui/fulcro/inspect/ui/db_explorer.cljs
+++ src/ui/fulcro/inspect/ui/db_explorer.cljs
@@ -1,50 +1,76 @@
 (ns fulcro.inspect.ui.db-explorer
   (:require
     [clojure.pprint :refer [pprint]]
-    [fulcro.client.primitives :as prim :refer [defsc]]
-    [fulcro.client.mutations :refer [defmutation]]
+    [clojure.set :as set]
     [fulcro.client.dom :as dom]
+    [fulcro.client.mutations :refer [defmutation]]
+    [fulcro.client.primitives :as prim :refer [defsc]]
     [fulcro.inspect.helpers :as h]
-    [clojure.set :as set]))
+    [fulcro.inspect.ui.core :as ui]
+    [fulcro.util :refer [ident?]]))
 
 (defmutation set-content [new-state]
   (action [env]
     (h/swap-entity! env assoc :current-database new-state)))
 
-(defsc EntityLevel [this {:keys [path entity]}]
+(defsc EntityLevel [this {:keys [entity selectIdent]}]
   {}
-  (dom/div
-    {}
-    (dom/pre {}
-      (with-out-str
-        (pprint entity)))))
+  (prim/fragment
+    (dom/tr
+      (dom/th "Key")
+      (dom/th "Value"))
+    (map
+      (fn [[k v]]
+        (dom/tr
+          (dom/td (str k))
+          (dom/td
+            (cond
+              (ident? v) (dom/button {:onClick #(when selectIdent (selectIdent v))}
+                           (str v))
+              (nil? v) "nil"
+              :else (str v)))))
+      entity)))
 
 (def ui-entity-level (prim/factory EntityLevel))
 
 (defsc TableLevel [this {:keys [entity-ids selectEntity]}]
   {}
-  (map-indexed
-    (fn [idx entity-id]
-      (dom/div {:key (str idx)}
-        (dom/button {:onClick #(when selectEntity (selectEntity entity-id))}
-          (pr-str entity-id))
-        (dom/button {} "Add this entity to Watch list")))
-    entity-ids))
+  (prim/fragment
+    (dom/tr
+      (dom/th "Entity ID"))
+    (map
+      (fn [entity-id]
+        (dom/tr
+          (dom/td
+            (dom/button {:onClick #(when selectEntity (selectEntity entity-id))}
+              (pr-str entity-id)))))
+      entity-ids)))
 
 (def ui-table-level (prim/factory TableLevel))
 
 (defsc TopLevel [this {:keys [tables root-values selectTopKey]}]
   {}
-  (dom/div
-    (dom/pre
-      (with-out-str
-        (pprint root-values)))
-    (map-indexed
-      (fn [idx k]
-        (dom/div {:key (str idx)}
-          (dom/button {:onClick #(when selectTopKey (selectTopKey k))}
-            (pr-str k))))
-      tables)))
+  (prim/fragment
+    (dom/tr
+      (dom/th "Table")
+      (dom/th "Entities"))
+    (map
+      (fn [[k v]]
+        (dom/tr
+          (dom/td (str k))
+          (dom/td
+            (dom/button {:onClick #(when selectTopKey (selectTopKey k))}
+              (str "Show " (count v) " ...")))))
+      (sort tables))
+    (dom/tr
+      (dom/th "Key")
+      (dom/th "Value"))
+    (map
+      (fn [[k v]]
+        (dom/tr
+          (dom/td (str k))
+          (dom/td (str v))))
+      root-values)))
 
 (def ui-top-level (prim/factory TopLevel))
 
@@ -57,12 +83,8 @@
         reconciler (prim/any->reconciler this)]
     (prim/transact! reconciler [::id id] `[(set-path {:path ~path})])))
 
-(defsc DBExplorer [this {:ui/keys [path]
-                         ::keys   [id]
-                         :keys    [current-database]}]
-  {:query         [:ui/path
-                   ::id
-                   :current-database]
+(defsc DBExplorer [this {:ui/keys [path] :keys [current-database]}]
+  {:query         [:ui/path ::id :current-database]
    ;   :initLocalState (fn [] {:selectTopKey (fn [k] (select-top-key this k))})
    :ident         [::id ::id]
    :initial-state {:current-database {}}}
@@ -73,33 +95,47 @@
                (= 2 (count path)) :entity)]
     (dom/div {}
       (case mode
-        :top (dom/div {} (dom/button "TOP"))
+        :top (dom/div {}
+               (dom/button "TOP"))
         :table (dom/div {}
-                 (dom/button {:onClick #(set-path! this [])} "TOP")
-                 (dom/button {} (str (first path))))
+                 (dom/button {:onClick #(set-path! this [])}
+                   "TOP")
+                 ">"
+                 (dom/button {:disabled true}
+                   (str (first path))))
         :entity (dom/div {}
-                  (dom/button {:onClick #(set-path! this [])} "TOP")
-                  (dom/button {} (str (second path)))))
-      (case mode
-        :top (let [top-keys    (set (sort (keys current-database)))
-                   tables      (filter
-                                 (fn [k]
-                                   (let [v (get current-database k)]
-                                     (and
-                                       (map? v)
-                                       (every? map? (vals v)))))
-                                 top-keys)
-                   root-values (select-keys current-database (set/difference top-keys (set tables)))]
-               (ui-top-level {:tables       tables
-                              :root-values  root-values
-                              :selectTopKey (fn [k] (set-path! this [k]))}))
-        :table (ui-table-level {:entity-ids   (or
-                                                (sort (keys (get-in current-database path)))
-                                                [])
-                                :selectEntity (fn [id]
-                                                (set-path! this (conj path id)))})
-        :entity (ui-entity-level {:x      (random-uuid)
-                                  :entity (get-in current-database path)})
-        (dom/div "Internal Error")))))
+                  (dom/button {:onClick #(set-path! this [])}
+                    "TOP")
+                  ">"
+                  (dom/button {:onClick #(set-path! this [(first path)])}
+                    (str (first path)))
+                  ">"
+                  (dom/button {:disabled true}
+                    (str (second path)))))
+      ;(ui/icon :remove_red_eye)
+      (dom/table {}
+        (dom/tbody
+          (case mode
+            :top (let [top-keys    (set (sort (keys current-database)))
+                       tables      (filter
+                                     (fn [k]
+                                       (let [v (get current-database k)]
+                                         (and
+                                           (map? v)
+                                           (every? map? (vals v)))))
+                                     top-keys)
+                       root-values (select-keys current-database (set/difference top-keys (set tables)))]
+                   (ui-top-level {:tables       (select-keys current-database tables)
+                                  :root-values  root-values
+                                  :selectTopKey (fn [k] (set-path! this [k]))}))
+            :table (ui-table-level {:entity-ids   (or
+                                                    (sort (keys (get-in current-database path)))
+                                                    [])
+                                    :selectEntity (fn [id]
+                                                    (set-path! this (conj path id)))})
+            :entity (ui-entity-level {:entity (get-in current-database path)
+                                      :selectIdent (fn [ident]
+                                                     (set-path! this ident))})
+            (dom/div "Internal Error")))))))
 
 (def ui-db-explorer (prim/factory DBExplorer))
