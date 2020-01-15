00ccadf7fc16e8415682a4d8767f72e68465cd05
Anthony D'Ambrosio <anthony.dayzerostudio@gmail.com>
2020-01-15 14:21:42 -0800

    styling settings

--- src/ui/fulcro/inspect/ui/settings.cljs
+++ src/ui/fulcro/inspect/ui/settings.cljs
@@ -13,15 +13,19 @@
   {:query         [::id :ui/websocket-port]
    :ident         [::id ::id]
    :initial-state (fn [_] {:ui/websocket-port (or (.get settings "port") 8237)})}
-  (div :.ui.container
-    (dom/div (dom/label "Websocket Port: ")
-      (dom/input {:value    websocket-port
+  (div :.ui.container {:style {:margin "1rem"}}
+    (div :.ui.form.big
+      (div :.fields
+        (div :.field.inline
+          (dom/label "Websocket Port: ")
+          (input {:value    websocket-port
                   :type     "number"
-                  :onChange #(m/set-integer! this :ui/websocket-port :event %)})
-      (dom/button {:onClick (fn []
-                              (.send ipcRenderer "event"
-                                #js {:port    websocket-port
-                                     :restart true}))}
-        "Restart Websockets"))))
+                  :onChange #(m/set-integer! this :ui/websocket-port :event %)}))
+        (button :.ui.button.primary.red
+          {:onClick (fn []
+                      (.send ipcRenderer "event"
+                        #js {:port    websocket-port
+                             :restart true}))}
+          (dom/span :.ui.text.large "Restart Websockets"))))))
 
 (def ui-settings (fp/factory Settings))
