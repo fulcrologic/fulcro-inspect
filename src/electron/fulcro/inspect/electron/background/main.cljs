e6f13ae9178903862de0cf30d1b74caf4d2626e4
Anthony D'Ambrosio <anthony.dayzerostudio@gmail.com>
2020-01-17 19:00:10 -0800

    debouncing window state saving

--- src/electron/fulcro/inspect/electron/background/main.cljs
+++ src/electron/fulcro/inspect/electron/background/main.cljs
@@ -4,31 +4,33 @@
     ["path" :as path]
     ["electron-settings" :as settings]
     ["url" :as url]
+    [goog.functions :as g.fns]
     [fulcro.inspect.electron.background.server :as server]))
 
 (defn get-setting [k default] (or (.get settings k) default))
 (defn set-setting! [k v] (.set settings k v))
 
 (defn save-state! [window]
-  (fn []
-    (mapv (fn [[k v]] (set-setting! (str "window-" k) v))
-      (js->clj (.getBounds window)))))
+  (mapv (fn [[k v]] (set-setting! (str "BrowserWindow/" k) v))
+    (js->clj (.getBounds window))))
+
+(defn debounce [ms f] (g.fns/debounce f ms))
 
 (defn create-window []
   (let [win (electron/BrowserWindow.
-              #js {:width          (get-setting "window-width" 800)
-                   :height         (get-setting "window-height" 600)
-                   :x              (get-setting "window-x" js/undefined)
-                   :y              (get-setting "window-y" js/undefined)
+              #js {:width          (get-setting "BrowserWindow/width" 800)
+                   :height         (get-setting "BrowserWindow/height" 600)
+                   :x              (get-setting "BrowserWindow/x" js/undefined)
+                   :y              (get-setting "BrowserWindow/y" js/undefined)
                    :webPreferences #js {:nodeIntegration true}})]
     (.loadURL win (url/format #js {:pathname (path/join js/__dirname ".." ".." "index.html")
                                    :protocol "file:"
                                    :slashes  "true"}))
     (.. win -webContents openDevTools)
     (doto win
-      (.on "resize" (save-state! win))
-      (.on "move" (save-state! win))
-      (.on "close" (save-state! win)))
+      (.on "resize" (debounce 500 #(save-state! win)))
+      (.on "move" (debounce 500 #(save-state! win)))
+      (.on "close" (debounce 500 #(save-state! win))))
     (server/start! (.-webContents win))))
 
 (defn init []
