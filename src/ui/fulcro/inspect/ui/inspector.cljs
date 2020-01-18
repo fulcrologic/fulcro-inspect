2c996fecd7f63d92cf840cbb511771e6a76829f0
Tony Kay <tony.kay@gmail.com>
2020-01-18 11:52:54 -0800

    hid less useful tabs

--- src/ui/fulcro/inspect/ui/inspector.cljs
+++ src/ui/fulcro/inspect/ui/inspector.cljs
@@ -37,7 +37,7 @@
       ::oge                  (fp/get-initial-state oge/OgeView {})
       ::transactions         (fp/get-initial-state transactions/TransactionList [])
       ::db-explorer          (fp/get-initial-state db-explorer/DBExplorer {})
-      ::settings             (fp/get-initial-state settings/Settings {})
+      ;::settings             (fp/get-initial-state settings/Settings {})
       :ui/more-open?         false})
 
    :ident
@@ -54,7 +54,7 @@
     {::transactions (fp/get-query transactions/TransactionList)}
     {::index-explorer (fp/get-query fiex/IndexExplorer)}
     {::oge (fp/get-query oge/OgeView)}
-    {::settings (fp/get-query settings/Settings)}]
+    #_{::settings (fp/get-query settings/Settings)}]
 
    :css
    [[:.container {:display        "flex"
@@ -125,13 +125,13 @@
       (dom/div :.tabs
         (tab-item {:title "DB" :page ::page-db})
         (tab-item {:title "DB Explorer" :page ::page-db-explorer})
-        (tab-item {:title "Element" :page ::page-element})
+        #_(tab-item {:title "Element" :page ::page-element})
         (tab-item {:title "Transactions" :page ::page-transactions})
         (tab-item {:title "Network" :page ::page-network})
         (tab-item {:title "Query" :page ::page-oge})
         (tab-item {:title "Index Explorer" :page ::page-index-explorer})
-        (tab-item {:title "i18n" :page ::page-i18n})
-        (tab-item {:title "Settings" :page ::page-settings})
+        #_(tab-item {:title "i18n" :page ::page-i18n})
+        #_(tab-item {:title "Settings" :page ::page-settings})
         (dom/div :.flex)
         #_(dom/div #js {:className (:more css)
                         :onClick   (fn [e]
@@ -168,7 +168,7 @@
           ::page-i18n
           (i18n/translations-viewer i18n)
 
-          ::page-settings
+          #_#_::page-settings
           (settings/ui-settings settings)
 
           (dom/div
