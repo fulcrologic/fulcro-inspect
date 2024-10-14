39baf8a5ea0cdf9248c851762be61cb6dea4c66e
Jakub Holy <jakubholy@jakubholy.net>
2024-10-14 23:28:21 +0200

    Upgrade manifest, service worker
    Building on Tony's work, I upgrade the manifest to v3 and update shadow build config to have 3 builds: for the content script, the service worker (replacing the old background.js), and the devtools UI.
    
    Currently, the whitespace-only release build of devtools fails to load b/c it tries to use goog.provide before it is defined - see https://clojurians.slack.com/archives/C6N245JGG/p1728940771737579

--- src/chrome/fulcro/inspect/chrome/background/main.cljs
+++ src/chrome/fulcro/inspect/chrome/background/main.cljs
@@ -14,18 +14,22 @@
     (gobj/getValueByKeys message "fulcro-inspect-devtool-message")
     (let [tab-id      (gobj/get message "tab-id")
           remote-port (get @remote-conns* tab-id)]
-      (.postMessage remote-port message))))
+      (when-not remote-port
+        (println "WARN: No stored remote port for this sender tab"
+                 tab-id
+                 "Known tabs:" (keys @remote-conns*))) ; FIXME rm
+      (some-> remote-port (.postMessage message)))))
 
 (defn set-icon-and-popup [tab-id]
-  (js/chrome.browserAction.setIcon
-    #js {:tabId tab-id
-         :path  #js {"16"  "icon-16.png"
-                     "32"  "icon-32.png"
-                     "48"  "icon-48.png"
-                     "128" "icon-128.png"}})
-  (js/chrome.browserAction.setPopup
-    #js {:tabId tab-id
-         :popup "popups/enabled.html"}))
+  #_(js/chrome.action.setIcon ; FIXME Fails to be fetched
+     #js {;:tabId tab-id
+          :path  #js {"16"  "icon-16.png"
+                      "32"  "icon-32.png"
+                      "48"  "icon-48.png"
+                      "128" "icon-128.png"}})
+  (js/chrome.action.setPopup
+   #js {:tabId tab-id
+        :popup "popups/enabled.html"}))
 
 (defn handle-remote-message [ch message port]
   (cond
@@ -43,41 +47,51 @@
     (let [tab-id (gobj/getValueByKeys port "sender" "tab" "id")]
       (set-icon-and-popup tab-id))))
 
-(js/chrome.runtime.onConnect.addListener
-  (fn [port]
-    (case (gobj/get port "name")
-      "fulcro-inspect-remote"
-      (let [background->devtool-chan (chan (async/sliding-buffer 50000))
-            listener                 (partial handle-remote-message background->devtool-chan)
-            tab-id                   (gobj/getValueByKeys port "sender" "tab" "id")]
+(defn add-listener []
+  (js/chrome.runtime.onConnect.addListener
+   (fn [port]
+     (case (gobj/get port "name")
+       "fulcro-inspect-remote"
+       (let [background->devtool-chan (chan (async/sliding-buffer 50000))
+             listener                 (partial handle-remote-message background->devtool-chan)
+             tab-id                   (gobj/getValueByKeys port "sender" "tab" "id")]
 
-        (swap! remote-conns* assoc tab-id port)
+         (swap! remote-conns* assoc tab-id port)
 
-        (.addListener (gobj/get port "onMessage") listener)
-        (.addListener (gobj/get port "onDisconnect")
-          (fn [port]
-            (.removeListener (gobj/get port "onMessage") listener)
-            (swap! remote-conns* dissoc tab-id)
-            (async/close! background->devtool-chan)))
+         (println "DEBUG onConn listener msg="
+                  (gobj/get port "name")
+                  "for tab-id" tab-id "storing the port...") ; FIXME rm
 
-        (go-loop []
-          (when-let [{:keys [tab-id message] :as data} (<! background->devtool-chan)]
+
+         (.addListener (gobj/get port "onMessage") listener)
+         (.addListener (gobj/get port "onDisconnect")
+                       (fn [port]
+                         (.removeListener (gobj/get port "onMessage") listener)
+                         (swap! remote-conns* dissoc tab-id)
+                         (async/close! background->devtool-chan)))
+
+         (go-loop []
+           (when-let [{:keys [tab-id message] :as data} (<! background->devtool-chan)]
             ; send message to devtool
-            (if (contains? @tools-conns* tab-id)
-              (do
-                (.postMessage (get @tools-conns* tab-id) message)
-                (recur))
-              (recur)))))
+             (if (contains? @tools-conns* tab-id)
+               (do
+                 (.postMessage (get @tools-conns* tab-id) message)
+                 (recur))
+               (recur)))))
 
-      "fulcro-inspect-devtool"
-      (let [listener (partial handle-devtool-message port)]
-        (.addListener (gobj/get port "onMessage") listener)
-        (.addListener (gobj/get port "onDisconnect")
-          (fn [port]
-            (.removeListener (gobj/get port "onMessage") listener)
-            (if-let [port-key (->> @tools-conns*
-                                   (keep (fn [[k v]] (if (= v port) k)))
-                                   (first))]
-              (swap! tools-conns* dissoc port-key)))))
+       "fulcro-inspect-devtool"
+       (let [listener (partial handle-devtool-message port)]
+         (.addListener (gobj/get port "onMessage") listener)
+         (.addListener (gobj/get port "onDisconnect")
+                       (fn [port]
+                         (.removeListener (gobj/get port "onMessage") listener)
+                         (if-let [port-key (->> @tools-conns*
+                                                (keep (fn [[k v]] (if (= v port) k)))
+                                                (first))]
+                           (swap! tools-conns* dissoc port-key)))))
 
-      (js/console.log "Ignoring connection" (gobj/get port "name")))))
+       (js/console.log "Ignoring connection" (gobj/get port "name"))))))
+
+(defn init []
+  (add-listener)
+  (js/console.log "Fulcro service worker init done"))
